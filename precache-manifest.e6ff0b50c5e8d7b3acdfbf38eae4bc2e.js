9061eaad73d235b06fe0b0cc7d2b0fb2d8379761
natalia <natalia.verstka@yandex.ru>
2019-01-15 19:40:35 +0300

    todo-list build

---
+++ precache-manifest.e6ff0b50c5e8d7b3acdfbf38eae4bc2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "086f0c46fb854c8227fd",
+    "url": "/static/js/main.086f0c46.chunk.js"
+  },
+  {
+    "revision": "c26583f2bc635d6b5c6e",
+    "url": "/static/js/1.c26583f2.chunk.js"
+  },
+  {
+    "revision": "086f0c46fb854c8227fd",
+    "url": "/static/css/main.602f894b.chunk.css"
+  },
+  {
+    "revision": "39c41936580d88016160e594ce296436",
+    "url": "/index.html"
+  }
+];
